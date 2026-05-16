7d8a04b07bfa0c5e90bed54876b8723bd7137369
yatsuna827 <yatsuna_827@yahoo.co.jp>
2026-05-16 23:09:55 +0900

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 import solid from 'vite-plugin-solid';
 
 export default defineConfig({
+  base: '/griddlerzen/',
   plugins: [solid()],
   test: {
     globals: false,
